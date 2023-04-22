909f47a086b4d502e4d82f58db4ff20de431a136
Brycies <brysonodhiambo29@gmail.com>
2023-04-22 13:09:25 +0300

    income tracker app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from Rreact;
+import header from './components/header';
+
+function App() {
+
+  const [income, setIncome] = useState([])
+  const [totalIncome, setTotalIncome] = useState (0)
+
+
+  return (
+  <div className="App">
+    <header totalIncome={totalIncome}/>
+  </div>
+  )
+}
+
+export default App;
