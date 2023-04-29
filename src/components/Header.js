d704346e48e6689e8f39427f843a1dcd0f9b6c9f
Brycies <brysonodhiambo29@gmail.com>
2023-04-29 21:51:28 +0300

    income

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header({ totalIncome }) {
   return (
     <header>
       <h1>Income Tracker</h1>
-      <div className="total-income">Ksh{totalIncome}</div>
+      <div className="total-income">US{totalIncome}</div>
     </header>
   );
 }
